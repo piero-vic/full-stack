ceebd187a3d6024b1c3e3b5f412d6629486dae21
Piero Lescano <pierolescano@protonmail.com>
2022-07-02 11:14:33 -0500

    feat: Add teapot path

--- app.js
+++ app.js
@@ -6,4 +6,10 @@ app.get('/', (req, res) => {
 	res.send('Hello There!');
 });
 
+app.get('/teapot', (req, res) => {
+  res.status(418)
+  res.set('X-Teapot', 'Yes, I\'m a teapot')
+	res.send('TEAPOT!!!');
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
